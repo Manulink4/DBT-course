7279cd59564ac953ed7d5baf121c1b68cc028a81
Manuel Nuño <mnunogar@gmail.com>
2022-11-20 23:32:08 +0000

    intermediate full dataset

--- models/marts/int_races.sql
+++ models/marts/int_races.sql
@@ -1,4 +1,3 @@
-
 with 
 
 results as (
@@ -11,26 +10,27 @@ drivers as (
 
 races as (
     select * from {{ ref('stg_f1_races') }}
+    
 ),
 
--- artists_normalized as (
---     select
---         artist_id,
---         max(artist_mbid) as artist_mbid,
---         max(artist_name) as name,
---         max(artist_familiarity) as familiarity,
---         max(artist_hotttnesss) as hotness,
---         count(*) as num_songs
---     from songs_artists
---     group by 1
+
+-- final as (
+--     select * from results 
+--     inner join drivers on results.driverId = drivers.driverId
+--     inner join races on results.raceId = races.raceId
+
 -- )
 
 final as (
-    select * from results
-        inner join drivers on results.driverId = drivers.driverId
-        inner join races on results.raceId = races.raceId
+    select results.resultId, results.raceId, results.constructorId, results.grid, 
+            results.position, results.points, results.laps, results.fastestLap, results.rank, results.time,
+            results.fastestLapTime, results.fastestLapSpeed, results.statusId, drivers.forename, drivers.surname, drivers.nationality, drivers.dob, 
+            races.year, races.round, races.name, races.date
+
+    from results
+    inner join drivers USING (driverId)
+    inner join races USING (raceId)
 
 )
-
 
 select * from final
